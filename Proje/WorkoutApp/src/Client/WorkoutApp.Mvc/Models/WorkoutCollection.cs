469f2c4e0d61bb937c66ac1d7082add46c33ad07
Musibra <mustafaibrahimaksuu@gmail.com>
2023-06-10 19:11:36 +0300

    "Ekle" button added to cards

--- Proje/WorkoutApp/src/Client/WorkoutApp.Mvc/Models/WorkoutCollection.cs
+++ Proje/WorkoutApp/src/Client/WorkoutApp.Mvc/Models/WorkoutCollection.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using WorkoutApp.DataTransferObjects.Responses;
 
 namespace WorkoutApp.Mvc.Models;
@@ -6,18 +7,29 @@ public class WorkoutCollection
 {
     public List<WorkoutItem> Workouts { get; set; } = new();
     public void ClearAll() => Workouts.Clear();
+    public int TotalWorkoutsCount => Workouts.Sum(i => i.Count);
+    public void AddNewWorkout(WorkoutItem workoutItem)
+    {
+        var existingWorkout = Workouts.FirstOrDefault(c => c.Workout.Id == workoutItem.Workout.Id);
+        if (existingWorkout != null)
+        {
+            existingWorkout.Count += workoutItem.Count;
+        }
+        else
+        {
+            Workouts.Add(workoutItem);
+        }
+    }
 }
 
 public class WorkoutItem
 {
-    public WorkoutItem(WorkoutDisplayResponse workout)
+    public WorkoutItem(WorkoutDisplayResponse workout, int count)
     {
-        Id = workout.Id;
-        Name = workout.Name;
-        Description = workout.Description;
+        Workout = workout;
+        Count = count;
     }
     
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string? Description { get; set; }
+    public WorkoutDisplayResponse Workout { get; set; }
+    public int Count { get; set; }
 }
